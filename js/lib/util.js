39bf6e3fe9be438b759ab0ab31e07b9698020c63
manxisuo <manxisuo@gmail.com>
2013-04-15 02:37:50 +0800

    highline

--- js/lib/util.js
+++ js/lib/util.js
@@ -7,8 +7,8 @@ function hightlineJsCode(codes) {
     result = result.replace(/("[^"]*")/g, "<span class='string'>$1</span>");
     
     // key word
-    var keyword_regexp = /(break|case|catch|continue|default|delete|do|else|finally|for|function|if|instanceof|in|new|return|switch|this|throw|try|typeof|var|void|while|with)\W/g;
-    result = result.replace(keyword_regexp, "<span class='keyword'>$1</span>");
+    var keyword_regexp = /((break|case|catch|continue|default|delete|do|else|finally|for|function|if|instanceof|in|new|return|switch|this|throw|try|typeof|var|void|while|with)(\W))/g;
+    result = result.replace(keyword_regexp, "<span class='keyword'>$2</span>$3");
     
     return result;
 }
